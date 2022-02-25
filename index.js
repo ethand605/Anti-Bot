26e37aa549d2c68ceaa228c01204276dd73c7095
Ethan Deng <ethand.2091@gmail.com>
2022-02-24 21:55:27 -0800

    something

--- index.js
+++ index.js
@@ -9,10 +9,15 @@ client.on("messageCreate", (message) => {
     if (message.author.bot) return;
     if (!message.content.startsWith(prefix)) return;
     message.reply(message.content.slice(prefix.length).toLowerCase());
+
+    console.log(message.guild.channels);
 });
 
-channel.messages.fetch({ limit: 10 })
-  .then(messages => console.log(`Received ${messages.size} messages`))
-  .catch(console.error);
+// channelName = 
+// const channel = new Discord.channel()
+
+// channel.messages.fetch({ limit: 10 })
+//   .then(messages => console.log(`Received ${messages.size} messages`))
+//   .catch(console.error);
 
 client.login(config.BOT_TOKEN);
