b1f0f2e67c0eeff2ba3d6ba7b1c403208d88951e
Ethan Deng <ethand.2091@gmail.com>
2022-02-25 15:09:21 -0800

    adding fetch all channels

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
 
+/*
+TODO:
+fetch from all channels:
+    write a function that accepts channel id then fetch
+capture include and exclude words 
+sort by date .createdTimeStamp
+add async to wait for fetch before displaying (chain resolve promises?)
+change from opening thread to embed with pagination
+*/
+
 const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
 
 const prefix = "/";
 const searchedWord = "122d";
-const excludedWords = ["role", "/"];
+const excludedWords = ["/", "role"];
 
 let exclusionCheck = (msg) => {
     found = false;
@@ -15,55 +25,64 @@ let exclusionCheck = (msg) => {
     return !found;
 }
 
+
+
 client.on("messageCreate", (message) => {
-    if (message.author.bot) return;
+    if (message.author.bot) return; 
     if (!message.content.startsWith(prefix)) return;
-    let channel = message.channel;
-
     
-
-    // message.reply(message.content.slice(prefix.length).toLowerCase());
-
     // let channel = message.channel;
-    let allMsg = [];
-    channel.messages.fetch({ limit: 100 })
-        .then( (messages) => {      
-            messages.forEach( (msg) => {
-                if (!msg.author.bot && msg.content.includes(searchedWord) && exclusionCheck(msg.content)){
-                     allMsg.push(`${msg.author.username}: ${msg.content} (${msg.url})\n`); 
-                }
-                
-            });
-            // console.log('Received Messages: \n'+ allMsg.join("\n"));
+
+    //fetch all channels
+    message.guild.channels.fetch()
+        .then( (channels) => {
+            let chanIDs = [];
+            channels.forEach( (channel) => {
+                if(channel.type==='GUILD_TEXT') chanIDs.push(channel.id);
+            })
+            return chanIDs;
         })
-        .catch(console.error);
+        .then( (chanIDs) => {
+            console.log(chanIDs.length);
+        })
+        /*
+        .then(fetch all messages)
+        .then(create thread)
+        */
     
-    channel.threads
-    .create({
-        name: `messages containing ${searchedWord} and excluding ${excludedWords.join(', ')}`,
-        autoArchiveDuration: 60,
-        reason: 'provide search result'
-    }).then( threadChannel => {
-        client.channels.fetch(threadChannel.id)
-        .then( replyThread => {
-            let len = allMsg.length;
-            replyThread.send('Messages found: \n');
-            //add async here so fetch in order?
-            for (let i = 0; i < len; i+=4){
-                replyThread.send(allMsg.slice(i,i+3).join('\n'));
-            }
-            let remain = len%3;
-            if (remain!=0) {replyThread.send(allMsg.slice(-remain).join('\n'));}
-        })
-        .catch(console.error);
-    }).catch(console.error)
+    // let allMsg = {};
+    // channel.messages.fetch({ limit: 100 })
+    //     .then( (messages) => {      
+    //         messages.forEach( (msg) => {
+    //             if (!msg.author.bot && msg.content.includes(searchedWord) && exclusionCheck(msg.content)){
+    //                  allMsg[msg.createdTimestamp]=  `${msg.author.username}: ${msg.content} (${msg.url})\n`; 
+    //             }
+    //         });
+            
+    //     }).then( () => {
+    //         channel.threads
+    //             .create({
+    //                 name: `messages containing ${searchedWord} and excluding ${excludedWords.join(', ')}`,
+    //                 autoArchiveDuration: 60,
+    //                 reason: 'provide search result'
+    //             }).then( threadChannel => {
+    //                 client.channels.fetch(threadChannel.id)
+    //                 .then( replyThread => {
+    //                     replyThread.send('Messages found: \n');
+    //                     let times = Object.keys(allMsg);
+    //                     times = Array.from(times.sort().reverse());
+    //                     for (let i = 0;i<times.length;i++){
+    //                         replyThread.send(allMsg[times[i]]+'\n');
+    //                     }
+    //                 })
+    //                 .catch(console.error);
+    //             })
+    //             .catch(console.error);
+    
+    // }).catch(console.error)
+    
 });
 
-// channelName = 
-// const channel = new Discord.channel()
 
-// channel.messages.fetch({ limit: 10 })
-//   .then(messages => console.log(`Received ${messages.size} messages`))
-//   .catch(console.error);
 
 client.login(config.BOT_TOKEN);
